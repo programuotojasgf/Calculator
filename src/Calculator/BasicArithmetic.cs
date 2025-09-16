583a72e904c35ce56f66c0ce4286ad100faf875b
Gediminas Fridricas <programuotojasgf@gmail.com>
2025-09-16 13:30:32 +0300

    refactor(basic-arithmetic): generalize Sum and use expression-bodied member

--- src/Calculator/BasicArithmetic.cs
+++ src/Calculator/BasicArithmetic.cs
@@ -2,8 +2,5 @@ namespace Calculator;
 
 public static class BasicArithmetic
 {
-    public static int Sum(int left, int right)
-    {
-        throw new NotImplementedException();
-    }
+    public static int Sum(int left, int right) => left + right;
 }
